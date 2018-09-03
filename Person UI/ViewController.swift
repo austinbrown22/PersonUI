1babeafa4d18713e68a709206f0e91d86e90a93d
Austin Brown <abbaseball22@yahoo.com>
2018-09-03 17:40:16 -0500

    First commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Austin Brown on 9/3/18.
+//  Copyright © 2018 Austin Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
